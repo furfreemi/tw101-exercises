38214c062093341c29f335baf392da34e914168e
furfreemi <furfreemi@yahoo.com>
2017-01-24 16:55:01 -0500

    Exercise 3 complete

--- src/main/java/com/thoughtworks/tw101/exercises/exercise3/OddSum.java
+++ src/main/java/com/thoughtworks/tw101/exercises/exercise3/OddSum.java
@@ -3,6 +3,15 @@ package com.thoughtworks.tw101.exercises.exercise3;
 public class OddSum {
 
     public int of(int start, int end) {
-        return -1;
+        // If start is even: increment to begin at first odd number; else, begin at start
+        int current = (start % 2 == 0) ? start + 1 : start;
+        int sum = 0;
+
+        while (current < end){
+            sum += current;
+            current += 2;
+        }
+
+        return sum;
     }
 }
