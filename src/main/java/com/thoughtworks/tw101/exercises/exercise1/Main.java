6b00f4c157bbf1d7e01a00591609fd93d09cb448
furfreemi <furfreemi@yahoo.com>
2017-01-24 16:36:57 -0500

    Exercise 1 complete

--- src/main/java/com/thoughtworks/tw101/exercises/exercise1/Main.java
+++ src/main/java/com/thoughtworks/tw101/exercises/exercise1/Main.java
@@ -3,7 +3,15 @@ package com.thoughtworks.tw101.exercises.exercise1;
 // Exercise #1: Print all odd numbers from 1 to 100 and their sum.
 
 public class Main {
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
+        int sum = 0;
+
+        for (int count = 1; count <= 100; count += 2) {   // Could pull out magic numbers
+            sum += count;
+            System.out.println(count);
+        }
+
+        System.out.println(sum);
     }
 }
