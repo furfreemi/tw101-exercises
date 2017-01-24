1483d9fb7d7b1f02c323c0952245050803ed2ce2
furfreemi <furfreemi@yahoo.com>
2017-01-24 17:10:25 -0500

    Exercise 5 complete, exercise 3 handles switched start-end case

--- src/main/java/com/thoughtworks/tw101/exercises/exercise3/OddSum.java
+++ src/main/java/com/thoughtworks/tw101/exercises/exercise3/OddSum.java
@@ -3,6 +3,13 @@ package com.thoughtworks.tw101.exercises.exercise3;
 public class OddSum {
 
     public int of(int start, int end) {
+        // Switches endpoints if start falls after end
+        if (end < start){
+            int temp = end;
+            end = start;
+            start = temp;
+        }
+
         // If start is even: increment to begin at first odd number; else, begin at start
         int current = (start % 2 == 0) ? start + 1 : start;
         int sum = 0;
